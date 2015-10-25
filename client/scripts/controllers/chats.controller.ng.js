5d32cbb199e677d4ac1cb70b965b86d663595171
Peter C <peter.chau.esq@gmail.com>
2015-10-25 03:24:32 +0000

    initial commit

---
+++ client/scripts/controllers/chats.controller.ng.js
@@ -0,0 +1,12 @@
+angular
+    .module('Whatsapp')
+    .controller('ChatsCtrl', ChatsCtrl);
+    
+function ChatsCtrl ($scope) {
+    $scope.chats = $scope.$meteorCollection(Chats, false);
+    $scope.remove = remove;
+    
+    function remove(chat) {
+        $scope.chats.remove(chat);
+    }    
+}
